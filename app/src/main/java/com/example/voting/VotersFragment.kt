49da4d8f4a6900ba617e78d8b31350af8cb33776
jolgo60 <jlgo60@hotmail.com>
2021-05-19 05:46:08 -0500

    Se elimino codigo de VotersFrament

--- app/src/main/java/com/example/voting/VotersFragment.kt
+++ app/src/main/java/com/example/voting/VotersFragment.kt
@@ -25,76 +25,7 @@ class VotersFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_voters, container, false)
 
-        binding = FragmentVotersBinding.bind(view)
-
-        binding.imageView2.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                if (ContextCompat.checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.READ_EXTERNAL_STORAGE
-                    ) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    openGallery()
-                } else {
-                    val permissionRequest = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    requestPermissions(permissionRequest, REQUES_PERMISSION_CODE)
-                }
-
-            } else {
-                openGallery()
-            }
-
-        }
-
         return view
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_GALLERY) {
-            if (resultCode == RESULT_OK && data != null) {
-                val photo = data.data
-                binding.imageView2.setImageURI(photo)
-            }else{
-                Toast.makeText(context, "You didn't any photo", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            REQUES_PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openGallery()
-                } else {
-                    Toast.makeText(
-                        context,
-                        "Unable to update location without permission",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-            else -> {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            }
-        }
-    }
-
-
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "image/*"
-        startActivityForResult(intent, REQUEST_IMAGE_GALLERY)
-    }
-
-    companion object {
-        private const val REQUEST_IMAGE_GALLERY = 100
-        private const val REQUES_PERMISSION_CODE = 101
-    }
 }
