3bc98e951be2122eeb28ca6db00c0ac87f1c4676
jolgo <jlgo60@hotmail.com>
2021-07-22 06:27:35 -0500

    Se agrego metodo para actulizar los datos en UpdateFragmen

--- app/src/main/java/com/example/voting/data/entities/Voters.kt
+++ app/src/main/java/com/example/voting/data/entities/Voters.kt
@@ -1,8 +1,15 @@
 package com.example.voting.data.entities
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+/**
+ * Metodo Parcelize y Parcelable permiten pasar los parametros
+ * de la clase Voters al UpdateFrament, necesario para actualizaar los datos
+ */
+@Parcelize
 @Entity(tableName = "voters_table")
 data class Voters(
     @PrimaryKey(autoGenerate = true)
@@ -11,4 +18,4 @@ data class Voters(
     val lastName: String,
     val votingCard: String,
     val img: String
-)
+):Parcelable
--- app/src/main/java/com/example/voting/list/ListAdapter.kt
+++ app/src/main/java/com/example/voting/list/ListAdapter.kt
@@ -3,42 +3,49 @@ package com.example.voting.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.voting.R
 import com.example.voting.data.entities.Voters
 import kotlinx.android.synthetic.main.custom_row.view.*
 
-class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     private var userList = emptyList<Voters>()
 
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {}
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false))
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false)
+        )
     }
 
     override fun getItemCount(): Int = userList.size
-       // return userList.size
+    // return userList.size
 
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = userList[position]
-       //holder.itemView.tvId.text = currentItem.votersId.toString()
+        //holder.itemView.tvId.text = currentItem.votersId.toString()
         holder.itemView.tvFirstName.text = currentItem.firstName
         holder.itemView.tvLastName.text = currentItem.lastName
         holder.itemView.tvAge.text = currentItem.votingCard
 
+        //Se usa Glide para que se proyecte la imagen en el RecyclerView
         Glide.with(holder.itemView.context).load(currentItem.img).into(holder.itemView.imageView3)
 
-       // Glide.with(holder.itemView.context).load(currentItem.img).into(holder.itemView.imageView2)
+        //Metodo para actualizar información del usuario (cuando demos click en el recycler view se mantenga la información)
+        holder.itemView.rowLayout.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToUpDateFragment(currentItem)
+            holder.itemView.findNavController().navigate(action)
+        }
 
-        
     }
 
-    fun setData(user: List<Voters>){
+    fun setData(user: List<Voters>) {
         this.userList = user
         notifyDataSetChanged()
     }
