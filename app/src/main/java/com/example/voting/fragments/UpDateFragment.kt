51044a37ef69177ef6bb50225cc690d2709dd85a
jolgo <jlgo60@hotmail.com>
2021-08-30 05:47:57 -0500

    Se corrigio error al actualizar los datos de usuario y la imagen no

--- app/src/main/java/com/example/voting/fragments/UpDateFragment.kt
+++ app/src/main/java/com/example/voting/fragments/UpDateFragment.kt
@@ -17,7 +17,9 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.voting.R
 import com.example.voting.data.UserViewModel
+import com.example.voting.data.entities.User
 import com.example.voting.data.entities.Voters
+import com.example.voting.databinding.FragmentUpDateBinding
 import kotlinx.android.synthetic.main.fragment_up_date.*
 import kotlinx.android.synthetic.main.fragment_up_date.view.*
 import java.io.File
@@ -27,49 +29,118 @@ import java.io.IOException
 
 class UpDateFragment : Fragment() {
 
+    private var _binding: FragmentUpDateBinding? = null
+    private val binding get() = _binding!!
 
     private val arg by navArgs<UpDateFragmentArgs>()
     private val mUserViewModel by viewModels<UserViewModel>()
+    var stringPath: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_up_date, container, false)
+    ): View {
+        _binding = FragmentUpDateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        view.etUpDateFirstName.editText?.setText(arg.currentUser.firstName)
-        view.etUpDateLastName.editText?.setText(arg.currentUser.lastName)
-        view.etUpdateNumerCard.editText?.setText(arg.currentUser.votingCard)
-        view.imageViewUpDate.setImageURI(Uri.parse(arg.currentUser.img))
 
-        view.buttonUpDate.setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.etUpDateFirstName.editText?.setText(arg.currentUser.firstName)
+        binding.etUpDateLastName.editText?.setText(arg.currentUser.lastName)
+        binding.etUpdateNumerCard.editText?.setText(arg.currentUser.votingCard)
+        binding.imageViewUpDate.setImageURI(Uri.parse(arg.currentUser.img))
+
+
+        binding.buttonUpDate.setOnClickListener {
             updateItem()
         }
 
-        return view
+        binding.imageViewUpDate.setOnClickListener {
+            openGallery()
+        }
+    }
 
+    //Open gallery
+    fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK && requestCode == 1) {
+                //File path
+                val selectedImageUri: Uri? = data?.data
+
+                //Convert Uri to a Bitmap
+                val source = selectedImageUri?.let {
+                    context?.let { it1 ->
+                        ImageDecoder.createSource(
+                            it1.contentResolver, it
+                        )
+                    }
+                }
+
+                val bitmap = source?.let { ImageDecoder.decodeBitmap(it) }
+                binding.imageViewUpDate.setImageBitmap(bitmap)
+
+                //Creation date
+                //File creation
+                val fileCreation = System.currentTimeMillis()
+                val imageName = ("$fileCreation${"image.jpg"}")
+
+                //If it does not exist create the temporary
+                val file = File(context?.getExternalFilesDir("PhotoTemp/"), imageName)
+                if (file.exists()) {
+                    file.delete()
+                }
+                //Save the temporary
+                try {
+                    val out = FileOutputStream(file)
+                    bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, out)
+                    out.flush()
+                    out.close()
+
+                } catch (e: IOException) {
+
+                }
+                stringPath = file.toString()
+
+            } else {
+                Toast.makeText(context, "You didn't any photo", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun updateItem() {
-        val firstName = etUpDateFirstName.editText?.text.toString()
-        val lastName = etUpDateLastName.editText?.text.toString()
-        val votingCard = etUpdateNumerCard.editText?.text.toString()
-        //val myFile = File(stringPath).toString()
+        val firsName = binding.etUpDateFirstName.editText?.text.toString()
+        val lastName = binding.etUpDateLastName.editText?.text.toString()
+        val votingCard  = binding.etUpdateNumerCard.editText?.text.toString()
+        var myFile = File(stringPath).toString()
+        //Validation of the image in case user data is updated and the image is not modified
+        if (myFile == "") {
+            myFile = Uri.parse(arg.currentUser.img).toString()
+        }
 
-        if (inputCheck(firstName,lastName,votingCard)){
+        if (inputCheck(firsName, lastName, votingCard)) {
 
             //Create User Object
-            val updateUser = Voters(arg.currentUser.votersId, firstName, lastName, votingCard, "")
+            val updateUser = Voters(arg.currentUser.votersId, firsName, lastName,votingCard, myFile )
             // Update Current User
             mUserViewModel.updateVoter(updateUser)
             Toast.makeText(context, "Updated Successfully!", Toast.LENGTH_SHORT).show()
             // Navigate Back
             findNavController().navigate(R.id.action_upDateFragment_to_listFragment)
-        }else {
+        } else {
             Toast.makeText(context, "Please fill out all fields.", Toast.LENGTH_SHORT).show()
         }
     }
+
 
     private fun inputCheck(firsName: String, lastName: String, numberCard: String): Boolean {
         return !(TextUtils.isEmpty(firsName) && TextUtils.isEmpty(lastName) && TextUtils.isEmpty(
@@ -77,5 +148,9 @@ class UpDateFragment : Fragment() {
         ))
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
